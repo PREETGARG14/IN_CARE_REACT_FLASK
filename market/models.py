e3b6f39f129cb0892b67c4474d1c8ca1a7820154
PREETGARG14 <preetgarg14@gmail.com>
2022-01-13 11:31:20 +0530

    chore:change in code

--- market/models.py
+++ market/models.py
@@ -1,6 +1,9 @@
+from sqlalchemy.orm import backref, relationship
 from market import db,login_manager
 
 from flask_login import UserMixin
+from datetime import datetime
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -13,14 +16,15 @@ class User(db.Model,UserMixin):
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
     password_hash = db.Column(db.String(length=600), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=0)
-    items = db.relationship('Item', backref='owned_user', lazy=True)    
+    # items = db.relationship('Item', backref='owned_user', lazy=True)
+    can_view_past_history=db.relationship('past_history_of_illness',backref='user', lazy=True)    
 
-    @property
-    def prettier_budget(self):
-        if len(str(self.budget)) >= 4:
-            return f'Rs {str(self.budget)[:-3]},{str(self.budget)[-3:]}'
-        else:
-            return f"Rs {self.budget}"
+    # @property
+    # def prettier_budget(self):
+    #     if len(str(self.budget)) >= 4:
+    #         return f'Rs {str(self.budget)[:-3]},{str(self.budget)[-3:]}'
+    #     else:
+    #         return f"Rs {self.budget}"
 
     @property
     def password(self):
@@ -39,25 +43,25 @@ class User(db.Model,UserMixin):
     def can_sell(self, item_obj):
         return item_obj in self.items
 
-class Item(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(length=30), nullable=False, unique=True)
-    price = db.Column(db.Integer(), nullable=False)
-    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
-    description = db.Column(db.String(length=1024), nullable=False, unique=True)
-    owner= db.Column(db.Integer(),db.ForeignKey('users.id'))
-    def __repr__(self):
-        return f'Item {self.name}'
+# class Item(db.Model):
+#     id = db.Column(db.Integer(), primary_key=True)
+#     name = db.Column(db.String(length=30), nullable=False, unique=True)
+#     price = db.Column(db.Integer(), nullable=False)
+#     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+#     description = db.Column(db.String(length=1024), nullable=False, unique=True)
+#     owner= db.Column(db.Integer(),db.ForeignKey('users.id'))
+#     def __repr__(self):
+#         return f'Item {self.name}'
 
-    def buy(self, user):
-        self.owner = user.id
-        user.budget += self.price
-        db.session.commit()
+#     def buy(self, user):
+#         self.owner = user.id
+#         user.budget += self.price
+#         db.session.commit()
 
-    def sell(self, user):
-        self.owner = None
-        user.budget -= self.price
-        db.session.commit()
+#     def sell(self, user):
+#         self.owner = None
+#         user.budget -= self.price
+#         db.session.commit()
 
 
 class AdminUser(db.Model, UserMixin):
@@ -67,5 +71,18 @@ class AdminUser(db.Model, UserMixin):
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
     password_hash = db.Column(db.String(length=60), nullable=False)
 
+class past_history_of_illness:
+    __tablename__='past_history'
+    id=db.Column(db.Integer(), primary_key=True)
+    problem = db.Column(db.String(length=30), nullable=False)
+    body_site=db.Column(db.String(length=30), nullable=False)
+    dateTime = db.Column(db.DateTime, nullable = False)
+    severity = db.Column(db.String(length=30), nullable=False)
+    last_updated=db.Column(db.DateTime, nullable = False)
+    user_id=db.Column(db.Integer(),db.ForeignKey('users.id'))
+    user=db.relationship('User  ',backref='past_history')   
+
+
+    
 db.create_all()
 db.session.commit()
--- market/routes.py
+++ market/routes.py
@@ -47,23 +47,23 @@ def market_page():
     selling_form = SellItemForm()
     if request.method == "POST":
         #Purchase Item Logic
-        purchased_item = request.form.get('purchased_item')
-        p_item_object = Item.query.filter_by(name=purchased_item).first()
-        if p_item_object:
-            if current_user.can_purchase(p_item_object):
-                p_item_object.buy(current_user)
-                flash(f"Congratulations! You purchased {p_item_object.name} for Rs {p_item_object.price}", category='success')
-            else:
-                flash(f"Unfortunately, you don't have enough money to purchase {p_item_object.name}!", category='danger')
-        #Sell Item Logic
-        sold_item = request.form.get('sold_item')
-        s_item_object = Item.query.filter_by(name=sold_item).first()
-        if s_item_object:
-            if current_user.can_sell(s_item_object):
-                s_item_object.sell(current_user)
-                flash(f"Congratulations! You sold {s_item_object.name} back to market!", category='success')
-            else:
-                flash(f"Something went wrong with selling {s_item_object.name}", category='danger')
+        # purchased_item = request.form.get('purchased_item')
+        # p_item_object = Item.query.filter_by(name=purchased_item).first()
+        # if p_item_object:
+        #     if current_user.can_purchase(p_item_object):
+        #         p_item_object.buy(current_user)
+        #         flash(f"Congratulations! You purchased {p_item_object.name} for Rs {p_item_object.price}", category='success')
+        #     else:
+        #         flash(f"Unfortunately, you don't have enough money to purchase {p_item_object.name}!", category='danger')
+        # #Sell Item Logic
+        # sold_item = request.form.get('sold_item')
+        # s_item_object = Item.query.filter_by(name=sold_item).first()
+        # if s_item_object:
+        #     if current_user.can_sell(s_item_object):
+        #         s_item_object.sell(current_user)
+        #         flash(f"Congratulations! You sold {s_item_object.name} back to market!", category='success')
+        #     else:
+        #         flash(f"Something went wrong with selling {s_item_object.name}", category='danger')
 
         return redirect(url_for('market_page'))
 
