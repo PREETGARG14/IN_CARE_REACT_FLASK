9410e6046f1e5cb321d2c0029962d1f18243b945
Siddthanth Chopra <siddhukanu1@gmail.com>
2022-01-14 11:50:14 +0530

    Minor Changes

--- market/routes.py
+++ market/routes.py
@@ -242,6 +242,6 @@ def edit_patient_page(page_id):
     if form.errors != {}: #If there are not errors from the validations
         for err_msg in form.errors.values():
             flash(f'There was an error with creating a user: {err_msg}', category='danger')
-    return render_template('edit_details.html', past_history = past_history , form = form)
+    return render_template('edit_details.html', past_history = past_history)
 
       
