7477308be1768376b9b451b136f4d61a5fbf8a5f
pawan9116 <pawanraip4@gmail.com>
2022-01-22 19:25:56 +0530

    fix backgroundImage

---
+++ build/precache-manifest.d1286cbab1ac45b20aef68a61ae89b06.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1050666cd3e16605a4947b3e60bc7de7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e4d36639b024cd81b08",
+    "url": "/static/css/main.cbca84f7.chunk.css"
+  },
+  {
+    "revision": "3b28a3a09209a46dda32",
+    "url": "/static/js/2.74175e75.chunk.js"
+  },
+  {
+    "revision": "40a2d59f70f374bff168",
+    "url": "/static/js/3.7279518d.chunk.js"
+  },
+  {
+    "revision": "34d7123485413f35ef58",
+    "url": "/static/js/4.b6a3b548.chunk.js"
+  },
+  {
+    "revision": "564b60104bbe33297f8d",
+    "url": "/static/js/5.db0769ca.chunk.js"
+  },
+  {
+    "revision": "3e4d36639b024cd81b08",
+    "url": "/static/js/main.58ee64b3.chunk.js"
+  },
+  {
+    "revision": "80a54ff040ac613e06ce",
+    "url": "/static/js/runtime~main.649e1179.js"
+  },
+  {
+    "revision": "bdddb4d4999ce1cb8ce56e4e0bf50b83",
+    "url": "/static/media/dl.bdddb4d4.jpg"
+  },
+  {
+    "revision": "2ccc41f42bfe20f030ecfe44adc480a3",
+    "url": "/static/media/ul.2ccc41f4.jpg"
+  }
+]);
